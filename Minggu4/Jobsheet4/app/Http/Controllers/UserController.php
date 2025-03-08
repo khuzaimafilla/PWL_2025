7499f34d6917fd06773583bed02e3c3eaeb9704d
khuzaimafilla <khuzaimafilla@gmail.com>
2025-03-08 14:15:32 +0700

    Push Minggu4

---
+++ Minggu4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(){
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);        
+
+        // $user = UserModel::find(6);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {  
+        //     abort(404);  
+        // });
+        
+        // $user = UserModel::findOrFail(1);
+        // $user = UserModel::where('username', 'manager')->firstOrFail();
+
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ]
+        // );   
+        
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );        
+        // $user->save();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::create([
+        //     'username' => 'manager11',
+        //     'nama' => 'Manager11',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        
+        // $user->username = 'manager12';
+        
+        // $user->save();
+        
+        // $user->wasChanged(); // true
+        // $user->wasChanged('username'); // true
+        // $user->wasChanged(['username', 'level_id']); // true
+        // $user->wasChanged('nama'); // false
+        // dd($user->wasChanged(['nama', 'username'])); // true   
+        
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
+    }
+
+    // public function tambah(){
+    //     return view('user_tambah');
+    // }
+
+    // public function tambah_simpan(Request $request){
+    //     UserModel::create([
+    //         'username' => $request->username,
+    //         'nama' => $request->nama,
+    //         'password' => Hash::make($request->password),
+    //         'level_id' => $request->level_id
+    //     ]);
+
+    // return redirect('/user');
+    // }
+
+    // public function ubah($id){
+    //     $user = UserModel::find($id);
+    //     return view('user_ubah', ['data' => $user]);
+    // }
+
+    // public function ubah_simpan($id, Request $request){
+    //     $user = UserModel::find($id);
+
+    //     $user->username = $request->username;
+    //     $user->nama = $request->nama;
+    //     $user->password = Hash::make($request->password);
+    //     $user->level_id = $request->level_id;
+
+    //     $user->save();
+
+    //     return redirect('/user');
+    // }
+
+    // public function hapus($id){
+    //     $user = UserModel::find($id);
+    //     $user->delete();
+
+    //     return redirect('/user');
+    // }
+
+}
